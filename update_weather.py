90ec895e19fbd7e899d951b06427fc1f8ea21d5c
MPN7702 <mr_magoo21@hotmail.com>
2026-09-15 21:08:43 +0200

    Update update_weather.py

--- update_weather.py
+++ update_weather.py
@@ -403,7 +403,10 @@ for i in range(3):
     if date not in history[key]:
         history[key][date] = {}
 
-    history[key][date]["smhi"] = daily_summaryer["Torsby"]["smhi"]["hourly"],
+    history[key][date]["smhi"] = daily_summary(
+    weather["Torsby"]["smhi"]["hourly"],
+    date
+)
         date
     )
 
